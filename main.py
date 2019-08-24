115158d8649c87e7cf42822b60a8cc51a56a0d2d
Sameer Puri <sameer@users.noreply.github.com>
2019-08-24 10:01:31 -0500

    Update requirements.txt, capitalize global variables, add support for "if-needed shift entries", fix formatting changes due to new Pandas read_excel params

--- main.py
+++ main.py
@@ -15,62 +15,56 @@ def parse_doodle_datetime(datetime_str: str) -> datetime.datetime:
     return datetime.datetime.strptime(datetime_str, DOODLE_DATETIME_FORMAT)
 
 
-doodle_df = pd.read_excel('Doodle.xls', skiprows=3, header=[0, 1, 2])
+DOODLE_DF = pd.read_excel('Doodle.xls', skiprows=3, header=[0, 1, 2], skipfooter=1, index_col=0)
 
-shifts = 0
-mentors = []
-for i, series in doodle_df.iterrows():
-    if i == 'Count':
-        break
-    shifts = max(shifts, len(series))
-    mentors.append([j for j, val in enumerate(series.tolist()) if val == 'OK'])
+SHIFTS = 0
+MENTORS = []
+MENTORS_WEIGHTING = []
+for i, series in DOODLE_DF.iterrows():
+    SHIFTS = max(SHIFTS, len(series))
+    MENTORS.append([j for j, val in enumerate(series.tolist()) if val == 'OK' or val == '(OK)'])
+    MENTORS_WEIGHTING.append([1 if val == '(OK)' else 2 if val == 'OK' else 0 for val in series.tolist()])
 
-edges: List[List[cp.Variable]] = []
-constraints: List[cp.Constant] = []
+EDGES: List[List[cp.Variable]] = []
+CONSTRAINTS: List[cp.Constant] = []
 
-delta_shift: Dict[int, List[cp.Variable]] = {}
-for shift in range(0, shifts):
-    delta_shift[shift] = []
+delta_shift: Dict[int, List[cp.Variable]] = {shift: [] for shift in range(SHIFTS)}
 
-for mentor in mentors:
+for mentor in MENTORS:
     mentor_edges = []
-    for i in range(0, shifts):
+    for i in range(SHIFTS):
         edge = cp.Variable(boolean=True)
         mentor_edges.append(edge)
         delta_shift[i].append(edge)
         if i not in mentor:
-            constraints.append(edge == 0)
-    edges.append(mentor_edges)
+            CONSTRAINTS.append(edge == 0)
+    EDGES.append(mentor_edges)
 
-for delta in chain(delta_shift.values(), edges):
+for delta in chain(delta_shift.values(), EDGES):
     if len(delta) > 0:
-        constraints.append(cp.sum(delta) <= 1)
+        CONSTRAINTS.append(cp.sum(delta) <= 1)
 
-objective = 0
-for mentor_edges in edges:
-    for edge in mentor_edges:
-        objective += edge
-objective = cp.Maximize(objective)
+OBJECTIVE = 0
+for mentor_weighting, mentor_edges in zip(MENTORS_WEIGHTING, EDGES):
+    for weighting, edge in zip(mentor_weighting, mentor_edges):
+        OBJECTIVE += weighting * edge
+OBJECTIVE = cp.Maximize(OBJECTIVE)
 
-problem = cp.Problem(objective, constraints)
+PROBLEM = cp.Problem(OBJECTIVE, CONSTRAINTS)
 
-print(f'Attempting to assign {shifts} shifts to {len(mentors)} mentors')
-problem.solve()
+print(f'Attempting to assign {SHIFTS} shifts to {len(MENTORS)} mentors')
+PROBLEM.solve()
 
-if problem.status in ["infeasible", "unbounded"]:
+if PROBLEM.status in ["infeasible", "unbounded"]:
     print(
-        f'The solver has found the problem to be {problem.status}. Your input may be incorrectly formatted.')
+        f'The solver has found the problem to be {PROBLEM.status}. Your input may be incorrectly formatted.')
 else:
     print(
-        f'Shifts have been assigned with an optimal assignment of {round(problem.value)}')
+        f'Shifts have been assigned with an optimal assignment of {round(PROBLEM.value)}')
     assignments: List[Tuple[datetime.datetime, str]] = []
 
-    for i, (name, series) in enumerate(doodle_df.iterrows()):
-        if name == 'Count':
-            break
-
-        active_edges = [j for j in range(0, shifts) if round(
-            float(edges[i][j].value)) == 1]
+    for i, (name, series) in enumerate(DOODLE_DF.iterrows()):
+        active_edges = [j for j in range(SHIFTS) if round(float(EDGES[i][j].value)) == 1]
         if len(active_edges) > 0:
             for active_edge in active_edges:
                 shift_datetime_str = str(series.keys()[active_edge])
